880e3e42cd545e200eef7a1a4122351f84693516
Laptop-Salad <squaretacos456@gmail.com>
2021-04-11 08:44:48 +0100

    Adds basic functionality to the about page. Also changes the application title

--- app/src/main/java/com/example/color_guess/aboutActivity.kt
+++ app/src/main/java/com/example/color_guess/aboutActivity.kt
@@ -1,6 +1,8 @@
 package com.example.color_guess
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("ClassName")
@@ -8,5 +10,11 @@ class aboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val returnBtn: Button = findViewById(R.id.aboutReturn)
+        returnBtn.setOnClickListener{
+            intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
